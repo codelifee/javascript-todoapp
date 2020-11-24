1b3a9de3b56c196a29eea7ba586e11416c850580
codelifee <suminjung09@gmail.com>
2020-11-24 19:47:17 +0900

    Created completed functionality

--- app.js
+++ app.js
@@ -44,8 +44,15 @@ function addTodo(event) {
 function deleteCheck(e) {
     const item = e.target;
 
+    //DELETE TODO
     if(item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
         todo.remove();
+    }
+
+    //CHECKMARK
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 }
